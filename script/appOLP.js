e8dbd8f15856fbbf0b8ff0a73a6edd4c50199b57
Oluwaseye.Olofinyo <oluwaseye.olofinyo@andela.co>
2014-12-04 23:04:17 +0100

    gitHub2

---
+++ script/appOLP.js
@@ -0,0 +1,41 @@
+//This is the OLP format of the script, app.js. Both runs the project perfectly well.
+var repoSearch = {
+
+  gitHubAPI: "https://api.github.com/search/users",
+
+  submitForm: function() {
+    $("form").submit(function(evt){
+      evt.preventDefault();
+      var user = {
+      q: $('#search').val(),
+      sort: "repositories",
+      order: "desc"
+      };
+      $.getJSON(repoSearch.gitHubAPI, user, repoSearch.displayUsers());
+    });
+  },
+
+  displayUsers: function(){
+
+   var doIt =  function(data) {
+    var repoHTML = '<ul>';
+    console.log(data);
+    $.each(data.items, function(i, user) {
+      repoHTML += '<li class="grid-25 tablet-grid-50">';
+      repoHTML += '<a href="' + user.avatar_url + '" class="image">';
+      repoHTML += '<img src = "'+ user.avatar_url + '" class="image" width=200 height=200> </a>';
+      repoHTML +=  '<p> USER SCORE: ' + user.score +' </p>';
+      repoHTML +=  '<p> LOGIN: ' + user.login + '</p>'; 
+      repoHTML += '</li>';
+    });
+    repoHTML += '</ul>';
+    $('#options').html(repoHTML);
+  };
+
+  return doIt;
+  }
+};
+
+$(document).ready(function(){
+  repoSearch.submitForm();
+});
